c57207cccde0ef660898cbb971a4844c17876814
Preonzz <tomempis05@gmail.com>
2024-04-18 16:25:16 +0200

    Good jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,60 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     PlayerManager player; 
 
-    float inputHorizontal;
-    Vector2 playerVelocity;
     public Rigidbody2D body;
     public float vitesse = 10;
-    bool SurSol = false;
-    public Transform PositionPieds;
-    public float rayon;
-    public LayerMask sol;
-    bool EnSaut = false;
     float TimerSaut;
-    public float TempsSaut = 1.5f;
+    public float TempsSaut = 0.1f;
+    bool jumping = false;
 
     void Start()
     {
         player = GetComponent<PlayerManager>();
     }
-
-
-
-
-    // Start is called before the first frame update
-
     // Update is called once per frame
     void Update()
     {
-        SurSol = Physics2D.OverlapCircle(PositionPieds.position, rayon, sol);
         if (player.OnTheFloor == true && Input.GetButtonDown("Jump"))
         {
-            body.velocity = Vector2.up * player.jumpForce * 2;
-            player.OnTheFloor = true;
+            body.velocity = Vector2.up * player.jumpForce;
+            jumping = true;
             TimerSaut = TempsSaut;
         }
+        if (TimerSaut > 0)
+        {
+            TimerSaut -= Time.deltaTime;
+        }
 
-        if (player.OnTheFloor == true && Input.GetButton("Jump"))
+        if (jumping == true && Input.GetButton("Jump"))
         {
             if (TimerSaut > 0)
             {
                 body.velocity = Vector2.up * player.jumpForce * 2;
-                TimerSaut -= Time.deltaTime;
-            }
-
-            else
-            {
-                player.OnTheFloor = false;
             }
         }
-        if (Input.GetButtonUp("Jump"))
+        else
         {
-            player.OnTheFloor = false;
+            jumping = false;
+        }
+
+        if (jumping == false || TimerSaut <= 0)
+        {
+            body.AddForce(Vector2.up * player.fallSpeed);
         }
     }
     private void FixedUpdate()
