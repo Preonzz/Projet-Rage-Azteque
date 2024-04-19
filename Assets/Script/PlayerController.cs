b34cb3c1b46b845a95e57493d43a662f8c5de42f
MatthieuTardy <matthieu.tardy2007@gmail.com>
2024-04-19 17:17:51 +0200

    attack

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,14 +9,23 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D body;
     public float vitesse = 10;
+    public float MemoireVitesse;
     float TimerSaut;
     public float TempsSaut = 0.1f;
     bool jumping = false;
+    public float lastAxis = 1;
+    public Vector2 SpawnPosition;
+    GameObject smallAttack;
+    GameObject bigAttack;
+    bool inAttack = false;
 
     void Start()
     {
+        MemoireVitesse = vitesse;
         player = GetComponent<PlayerManager>();
         StartCoroutine(lightAttack());
+        StartCoroutine(rememberAxis());
+        StartCoroutine(HeavyAttack());
     }
     // Update is called once per frame
     void Update()
@@ -52,19 +61,69 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * vitesse, body.velocity.y);
-
-
     }
 
     IEnumerator lightAttack()
     {
-        yield return new WaitUntil(() => Input.GetButtonDown("Fire1"));
-        Vector2 SpawnPosition = new Vector2(transform.position.x, transform.position.y);
-        GameObject smallAttack = Instantiate(player.attaqueFaible, SpawnPosition, Quaternion.identity);
-        yield return new WaitForSecondsRealtime(0.5f);
+        yield return new WaitUntil(() => Input.GetButtonDown("Fire1") && inAttack == false);
+        if (lastAxis >= 0.1f)
+        {
+            SpawnPosition = new Vector2(transform.position.x+0.8f, transform.position.y);
+            smallAttack = Instantiate(player.attaqueFaible, SpawnPosition, Quaternion.identity);
+        }
+
+        if (lastAxis <= -0.1f)
+        {
+            SpawnPosition = new Vector2(transform.position.x-0.8f, transform.position.y);
+            smallAttack = Instantiate(player.attaqueFaible, SpawnPosition, Quaternion.identity);
+        }
+        inAttack = true;
+        vitesse = 1;
+        yield return new WaitForSecondsRealtime(0.2f);
         Destroy(smallAttack);
+        vitesse = MemoireVitesse;
+        yield return new WaitForSecondsRealtime(0.5f);
+        
+        inAttack = false;
         yield return null;
         StartCoroutine(lightAttack());
     }
+
+    IEnumerator HeavyAttack()
+    {
+        yield return new WaitUntil(() => Input.GetButtonDown("Fire2") && inAttack == false);
+        if (lastAxis >= 0.1f)
+        {
+            SpawnPosition = new Vector2(transform.position.x + 0.8f, transform.position.y);
+            bigAttack = Instantiate(player.attaqueForte, SpawnPosition, Quaternion.identity);
+        }
+
+        if (lastAxis <= -0.1f)
+        {
+            SpawnPosition = new Vector2(transform.position.x - 0.8f, transform.position.y);
+            bigAttack = Instantiate(player.attaqueForte, SpawnPosition, Quaternion.identity);
+        }
+        inAttack = true;
+        vitesse = 1;
+        yield return new WaitForSecondsRealtime(0.2f);
+        Destroy(bigAttack);
+        vitesse = MemoireVitesse;
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        inAttack = false;
+        yield return null;
+        StartCoroutine(HeavyAttack());
+    }
+
+    IEnumerator rememberAxis()
+    {
+        yield return new WaitUntil(() => Input.GetAxis("Horizontal") < -0.1 || Input.GetAxis("Horizontal") > 0.1);
+        if(Input.GetAxis("Horizontal") < -0.1 || Input.GetAxis("Horizontal") > 0.1)
+        {
+            lastAxis = Input.GetAxis("Horizontal");
+        }
+        StartCoroutine(rememberAxis());
+    }
+
 }
 
