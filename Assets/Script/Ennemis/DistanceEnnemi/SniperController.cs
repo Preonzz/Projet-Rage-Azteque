f50eabf7007e40d88a8b3bc6f3bf95e54aff9d7e
MatthieuTardy <matthieu.tardy2007@gmail.com>
2024-05-14 10:02:58 +0200

    bug camera

--- Assets/Script/Ennemis/DistanceEnnemi/SniperController.cs
+++ Assets/Script/Ennemis/DistanceEnnemi/SniperController.cs
@@ -32,6 +32,7 @@ public class SniperController : MonoBehaviour
         if (HP <= 0)
         {
             Destroy(gameObject);
+            GameManager.Instance.camera.isCameraShaking = false;
         }
     }
 
