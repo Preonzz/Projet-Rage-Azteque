9731b087bbd635cd60687630bf7618aedb906e73
Preonzz <tomempis05@gmail.com>
2024-05-20 13:42:31 +0200

    Autel Fini (manque graphismes)

--- Assets/Script/AutelScripts.cs
+++ Assets/Script/AutelScripts.cs
@@ -6,11 +6,6 @@ public class AutelScripts : MonoBehaviour
 {
     PlayerManager playerManager;
     public bool Active;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
--- Assets/Script/PowerChoiceScript.cs
+++ Assets/Script/PowerChoiceScript.cs
@@ -4,24 +4,65 @@ using UnityEngine;
 
 public class PowerChoiceScript : MonoBehaviour
 {
-    
+    PlayerManager player; 
+
+    public GameObject PowerChoiceMenu;
+
+    public GameObject SunBeamButton; 
+    public GameObject RageButton;
+    public GameObject HealButton;
     public void Autel()
     {
-        
+        Time.timeScale = 0f;
+        PowerChoiceMenu.SetActive(true);
+
+        SunBeamButton.SetActive(false);
+        RageButton.SetActive(false);
+        HealButton.SetActive(false);
+
+        if (GameManager.Instance.player.unlockSun == false)
+        {
+            Debug.Log("Sun not unlocked");
+            SunBeamButton.SetActive(true);
+        }
+        if (GameManager.Instance.player.unlockHeal == false)
+        {
+            HealButton.SetActive(true);
+        }
+        if (GameManager.Instance.player.unlockRage == false)
+        {
+            RageButton.SetActive(true);
+        }
     }
 
     public void unlockSunBeamButton()
     {
         GameManager.Instance.player.unlockSun = true;
+        CloseAutelMenu();
+
     }
     public void unlockRageButton()
     {
         GameManager.Instance.player.unlockRage = true;
+        CloseAutelMenu();
     }
 
 
     public void unlockHealButton()
     {
         GameManager.Instance.player.unlockHeal = true;
+        CloseAutelMenu();
+    }
+
+    public void CloseAutelMenu()
+    {
+
+        SunBeamButton.SetActive(false);
+        HealButton.SetActive(false);
+        RageButton.SetActive(false);
+
+        PowerChoiceMenu.SetActive(false);
+
+        Time.timeScale = 1f;
     }
 }
