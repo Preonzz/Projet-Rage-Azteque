bccc6e662513f40053c4fd9cf0c2a64388331c6c
Preonzz <tomempis05@gmail.com>
2024-05-21 20:56:22 +0200

    Fix Autel

--- Assets/Script/AutelScripts.cs
+++ Assets/Script/AutelScripts.cs
@@ -7,10 +7,12 @@ public class AutelScripts : MonoBehaviour
     PlayerManager playerManager;
     public bool Active;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Touché l'autel");
         if (other.gameObject.tag == "Player" && Active == true)
         {
+            Debug.Log("Activé");
             Active = false;
             GameManager.Instance.autelManager.Autel(); 
         }
