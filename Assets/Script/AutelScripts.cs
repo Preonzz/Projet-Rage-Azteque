20b2ecd8d8cb3410ba59b3a44a3f73b0bb651909
Preonzz <tomempis05@gmail.com>
2024-05-29 11:35:15 +0200

    Début Particule Autel

--- Assets/Script/AutelScripts.cs
+++ Assets/Script/AutelScripts.cs
@@ -6,12 +6,14 @@ public class AutelScripts : MonoBehaviour
 {
     PlayerManager playerManager;
     public bool Active;
+    public ParticleSystem particule;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player" && Active == true)
         {
             Active = false;
+            particule.Stop();
             GameManager.Instance.autelManager.Autel(); 
         }
     }
