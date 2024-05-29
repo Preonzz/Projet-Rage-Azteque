d4aa213022d8e61bc7929cd86612a1e86117da50
Preonzz <tomempis05@gmail.com>
2024-05-29 16:41:15 +0200

    Vidéo intro

--- Assets/Script/Menus/MainMenuButtons.cs
+++ Assets/Script/Menus/MainMenuButtons.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    public RawImage screen;
+    public Image etoile;
+    public Animator animator;
+    void Start()
+    {
+        StartCoroutine(video());
+        etoile.enabled = false;
+    }
     public void QuitButton()
     {
         Application.Quit();
@@ -12,5 +21,13 @@ public class MainMenuButtons : MonoBehaviour
     public void PlayButton()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level");
+    }
+
+    IEnumerator video()
+    {
+        yield return new WaitForSecondsRealtime(34);
+        Destroy(screen);
+        etoile.enabled = true;
+        animator.Play("Base Layer.disparition");
     }
 }
